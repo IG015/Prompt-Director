8b18dd071a9bd3df3db5294b0432cb500af509e7
IG015 <iago.rodrigues325@gmail.com>
2026-09-12 22:11:35 +0100

    Add browser-extension/background.js

---
+++ browser-extension/background.js
@@ -0,0 +1,5 @@
+import { installBridgeReceiver } from "./modules/bridgeReceiver.js";
+import { installSidePanelController } from "./modules/sidePanelController.js";
+
+installSidePanelController();
+installBridgeReceiver();
